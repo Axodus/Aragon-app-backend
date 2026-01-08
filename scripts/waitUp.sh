5bbe987c7d57a1019fdfee79f59aed7a10129a79
mzfshark <mzfshark@gmail.com>
2026-01-08 12:43:49 -0300

    feat: atualizar script waitUp.sh para melhorar a lógica de verificação do MongoDB PRIMARY

--- scripts/waitUp.sh
+++ scripts/waitUp.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-MONGO_URI=$1
+MONGO_URI="$1"
 
-echo "Waiting for MongoDB PRIMARY..."
+echo "Waiting for MongoDB PRIMARY at ${MONGO_URI}..."
 
 until mongosh "$MONGO_URI" --quiet --eval '
-  rs.isMaster().ismaster || rs.isMaster().isWritablePrimary
+  try {
+    rs.isMaster().ismaster || rs.isMaster().isWritablePrimary
+  } catch (e) {
+    false
+  }
 ' | grep -q true; do
   echo "MongoDB not PRIMARY yet..."
   sleep 2
