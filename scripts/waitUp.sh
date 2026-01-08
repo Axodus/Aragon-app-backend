6f593900d45c38f0b37ede074a1c3359008b4a32
mzfshark <mzfshark@gmail.com>
2026-01-08 19:08:37 -0300

    feat: atualizar lógica de dependências no docker-compose e simplificar script de espera

--- scripts/waitUp.sh
+++ scripts/waitUp.sh
@@ -3,14 +3,7 @@
 service_dns=$1
 service_port=$2
 
-function wait_up {
-
-  if ! nc -z ${service_dns} ${service_port}; then
-    echo "Waiting up for ${service_dns}..."
-    sleep 2
-    wait_up
-  fi
-
-}
-
-wait_up
+while ! nc -z -w 2 "${service_dns}" "${service_port}"; do
+  echo "Waiting up for ${service_dns}..."
+  sleep 2
+done
